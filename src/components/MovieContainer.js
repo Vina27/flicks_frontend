28f1ba6de58e63d8f79906b96618fae8bec87508
Vina27 <Mtabali27@gmail.com>
2020-10-10 12:44:08 -0400

    Created Components for Header, MovieContainer, MovieShowContainer and rendered them

---
+++ src/components/MovieContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieContainer extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is MovieContainer</p>
+            </div>
+        );
+    }
+}
+
+export default MovieContainer;
---
+++ src/components/MovieShowContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieShowContainer extends Component {
+    render() {
+        return (
+            <div>
+              <p>This is ShowContainer</p>  
+            </div>
+        );
+    }
+}
+
+export default MovieShowContainer;
